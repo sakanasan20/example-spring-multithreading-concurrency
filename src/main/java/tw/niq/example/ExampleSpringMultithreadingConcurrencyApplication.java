c63f012b860329155a0ab09ba50785e710082bba
Nick Chen <nick.chen@cmp.com.tw>
2024-01-09 14:58:31 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringMultithreadingConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMultithreadingConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMultithreadingConcurrencyApplication.class, args);
+	}
+
+}
